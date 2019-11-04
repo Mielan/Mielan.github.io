f63cb6cfdbe1089c80ef8c5ead5079fd5e3011da
Mielan <Mielan@users.noreply.github.com>
2019-11-03 21:46:00 -0800

    Update webgl1.js

--- js/webgl1.js
+++ js/webgl1.js
@@ -1,38 +1,120 @@
-
-var vertexShaderText = 
-[
-'precision mediump float;',
-'',
-'attribute vec2 vertPosition;',
-'',
-'v'
+var vertexShaderText = [
+    'precision mediump float;',
+    '',
+    'attribute vec2 vertPosition;',
+    '',
+    'void main()',
+    '{',
+    '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+    '}'
 ].join('\n');
 
+var fragmentShaderText = [
+    'precision mediump float;',
+    '',
+    'void main()',
+    '{',
+    '  gl_FragColor =  vec4(1.0, 0.0, 0.0, 1.0);',
+    '}'
+].join('\n');
 
-
-var initDemo = function(){
+var initDemo = function() {
     console.log('this is working');
 
     var canvas = document.getElementById('game-surface');
     var gl = canvas.getContext('webgl');
 
-    if (!gl){
+    if (!gl) {
         console.log('WebGL not supported, falling back on experimental gl');
         gl = cnavas.getContext('experimental-webgl');
     }
 
-    if (!gl){
+    if (!gl) {
         alert('Your browser does not support WebGL');
     }
 
+    /*
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
     gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+    */
 
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
-
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Create shaders
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+        return;
+    }
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+
+    // Create buffer
+    var triangleVerticies = [ // x, y
+        0.0, 0.5, -0.5, -0.5,
+        0.5, -0.5
+    ];
+
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+        positionAttribLocation, // attribute location
+        2, // number of elements per attribute
+        gl.FLOAT, // Type of elements
+        gl.FALSE,
+        2 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
+        0 // offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+
+    // Main render loop
+    /*
+    while(true){
+        updateWorld();
+        renderWorld();
+    }*/
+    /* JAVASCRIP VERSION
+    var loop = function(){
+        updateWorld();
+        renderWorld();
+        if(running){
+            requestAnimationFrame(loop);
+        }
+    }*/
+    gl.useProgram(program)
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
 };
 
 
@@ -45,7 +127,3 @@ function vertexShader(vertPosition, vertColor){
 
 };
 */
-
-
-
-
